0489ab3fb30c67270c74c5fcd05de2da085e482d
Jiali-Yuan <yuan.j@husky.neu.edu>
2020-05-24 00:17:55 -0700

    Adds variable firstWord comment

--- work/basic-js/compare.js
+++ work/basic-js/compare.js
@@ -5,10 +5,12 @@ module.exports = compare; // DO NOT MODIFY - USED FOR TESTING
 function compare( word, guess ) {  // DO NOT MODIFY
 
   /* YOU MAY MODIFY THE LINES BELOW */
+
   word = word.toLowerCase();
   guess = guess.toLowerCase();
 
-  let firstWord = {};
+  //The firstWorld object store the number of each character in 'word'.
+  let firstWord = {};  
   word.split('').forEach(function (letter) {
     if (!(letter in firstWord)) {
       firstWord[letter] = 0;
@@ -17,7 +19,6 @@ function compare( word, guess ) {  // DO NOT MODIFY
   });
 
   let count = 0;
-
   guess.split('').forEach(function (letter) {
     if (firstWord[letter] > 0) {
       count += 1;
