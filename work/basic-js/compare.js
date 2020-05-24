b806fc2b2cf9ea3ee7a4b3a900be007115edc15d
Jiali-Yuan <yuan.j@husky.neu.edu>
2020-05-24 00:09:08 -0700

    Submits basic-js assignment

--- work/basic-js/compare.js
+++ work/basic-js/compare.js
@@ -4,7 +4,25 @@ module.exports = compare; // DO NOT MODIFY - USED FOR TESTING
 
 function compare( word, guess ) {  // DO NOT MODIFY
 
-/* YOU MAY MODIFY THE LINES BELOW */
+  /* YOU MAY MODIFY THE LINES BELOW */
+  word = word.toLowerCase();
+  guess = guess.toLowerCase();
 
-  return 0; // this line is wrong
+  let firstWord = {};
+  word.split('').forEach(function (letter) {
+    if (!(letter in firstWord)) {
+      firstWord[letter] = 0;
+    }
+    firstWord[letter] += 1;
+  });
+
+  let count = 0;
+
+  guess.split('').forEach(function (letter) {
+    if (firstWord[letter] > 0) {
+      count += 1;
+      firstWord[letter] -= 1;
+    }
+  });
+  return count;
 }
