2d848b36bcda3c0fba9b3e79cc6ba8c4b4a26ab8
Jiali-Yuan <yuan.j@husky.neu.edu>
2020-07-10 16:56:11 -0700

    submit basic-react assignment

---
+++ work/basic-react/src/quizs.js
@@ -0,0 +1,25 @@
+
+export const quizs = [
+    {
+        question: "Name Jon Snow’s Direwolf.",
+        options: ["Lady", "Nymeria", "Ghost", "Catelyn Grey Wind" ],
+        answer: "Ghost"
+    },
+
+    {
+        question: "Name Arya's Direwolf.",
+        options: ["Shaggydog", "Grey Wind", "Lady", "Nymeria"],
+        answer: "Nymeria"
+    },
+
+    {
+        question: "What is Ser Davos' nickname?",
+        options: ["The Onion Knight", "Blackfish", "Littlefinger", "The Halfhand"],
+        answer: "The Onion Knight"
+    }
+];
+
+export function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+    return array;
+};
